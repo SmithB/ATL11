a82633753d6db52de074400558eb7c5be5415304
tsutterley <tsutterl@uw.edu>
2023-06-21 14:15:13 -0700

    add package and git utilities to version.py

--- setup.py
+++ setup.py
@@ -2,19 +2,23 @@ import os
 from setuptools import setup, find_packages
 
 # get long_description from README.md
-with open("README.md", "r") as fh:
+with open('README.md', mode='r', encoding='utf8') as fh:
     long_description = fh.read()
+long_description_content_type = "text/markdown"
 
+# get version
+with open('version.md', mode='r', encoding='utf8') as fh:
+    version = fh.read()
 
 # list of all scripts to be included with package
 scripts = [os.path.join('scripts',f) for f in os.listdir('scripts')]
 
 setup(
     name='ATL11',
-    version='2.0.0.0',
+    version=version,
     description='ASAS L3B Land Ice PGE',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type=long_description_content_type,
     url='https://github.com/suzanne64/ATL11',
     author='Ben Smith, Suzanne Murphy, Ben Jelley',
     author_email='besmith@uw.edu',
@@ -25,7 +29,7 @@ setup(
         'Topic :: Scientific/Engineering :: Physics',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.10',
     ],
     keywords='altimetry, ICESat-2, remote sensing',
     packages=find_packages(),
